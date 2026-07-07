7260dd0829ab27b73815bb77eda44817812fdae1
anandaputri01 <anandacantic@gmail.com>
2026-07-07 22:54:15 +0700

    upd

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://helmetwash.up.railway.app/'],
+    'allowed_origins' => ['https://helmetwash.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
